e3f0ab0ad6863d2f06882c0f1d7485b8551ff44d
qwe5507 <apvmffkdls@gmail.com>
2023-05-28 14:19:23 +0900

    init

---
+++ distributedRock/src/main/java/com/redis/distributedrock/DistributedRockApplication.java
@@ -0,0 +1,13 @@
+package com.redis.distributedrock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedRockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedRockApplication.class, args);
+    }
+
+}
---
+++ distributedRock/src/test/java/com/redis/distributedrock/DistributedRockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redis.distributedrock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedRockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
